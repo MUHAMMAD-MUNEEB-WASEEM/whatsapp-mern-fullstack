651838465686eb8455c35b5960478bad0ce60bb7
Muhammad Muneeb Waseem <muneebwaseem78@gmail.com>
2021-05-23 15:02:08 +0500

    basic app structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import Chat from './components/chat/Chat';
+import Sidebar from './components/sidebar/Sidebar';
 
 function App() {
   return (
     <div className="app">
       <h1>whatsapp clone</h1>
+
+      {/*Sidebar */}
+      <Sidebar/>
+
+      {/*Chat*/}
+      <Chat/>
     </div>
   );
 }
