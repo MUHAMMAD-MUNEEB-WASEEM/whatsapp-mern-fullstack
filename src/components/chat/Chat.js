5fc673352ffcc5273bebae1ced62661f05a4770c
Muhammad Muneeb Waseem <muneebwaseem78@gmail.com>
2021-05-28 16:57:57 +0500

    bump dummy chat messages and chat body styling

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -26,6 +26,37 @@ function Chat() {
                 </div>
             </div>
 
+            <div className="chat__body">
+
+                 <p className="chat__message">
+                     <span className="chat__name">Muneeb</span>
+                     
+                     This is a message
+                     
+                     <span className="chat__timestamp">
+                         {new Date().toUTCString()}
+                     </span>
+                </p>
+                <p className="chat__message chat__reciever">
+                     <span className="chat__name">Muneeb</span>
+                     
+                     This is a message
+                     
+                     <span className="chat__timestamp">
+                         {new Date().toUTCString()}
+                     </span>
+                </p>
+                <p className="chat__message">
+                     <span className="chat__name">Muneeb</span>
+                     
+                     This is a message
+                     
+                     <span className="chat__timestamp">
+                         {new Date().toUTCString()}
+                     </span>
+                </p>
+
+            </div>
         </div>
     )
 }
