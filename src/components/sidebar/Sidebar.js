8d52e6f859168e86c6bd4ac987b170e7d8b78ec5
Muhammad Muneeb Waseem <muneebwaseem78@gmail.com>
2021-05-28 15:37:07 +0500

    feat: build sidebar with dummy content

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,15 +1,43 @@
+import { Avatar, IconButton } from '@material-ui/core';
+import { DonutLarge, MoreVert, SearchOutlined } from '@material-ui/icons';
 import React from 'react'
+import ChatIcon from '@material-ui/icons/Chat';
 import './Sidebar.css';
+
+
+import image from '../images/my image3jpg.jpg' 
+import SidebarChat from './SidebarChat/SidebarChat';
 
 function Sidebar() {
     return (
         <div className="sidebar">
-            <h1>Sidebar</h1>
+           
             <div className="sidebar__header">
-                
+                <Avatar src={image}/>
                 <div className="sidebar__headerRight">
-
+                  <IconButton>
+                    <DonutLarge />
+                  </IconButton>
+                  <IconButton>
+                    <ChatIcon/>
+                  </IconButton>
+                  <IconButton>
+                    <MoreVert />
+                  </IconButton>
                 </div>
+            </div>
+                            
+            <div className="sidebar__search">
+                    <div className="sidebar__searchContainer">
+                      <SearchOutlined/>  
+                      <input type="text" placeholder="Search or start new chat"/>
+                    </div>
+            </div>
+
+            <div className="sidebar__chats">
+                <SidebarChat/>
+                <SidebarChat/>
+                <SidebarChat/>
             </div>
         </div>
     )
