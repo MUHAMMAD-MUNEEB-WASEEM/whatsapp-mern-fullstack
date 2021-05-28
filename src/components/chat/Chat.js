2e665eb465e6909714695d58b0bed9127d05a8aa
Muhammad Muneeb Waseem <muneebwaseem78@gmail.com>
2021-05-28 19:09:45 +0500

    add chat footer functionality

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,9 +1,13 @@
 import { Avatar, IconButton } from '@material-ui/core'
-import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons'
+import { AttachFile, InsertEmoticon, MicNone, MoreVert, SearchOutlined } from '@material-ui/icons'
 import React from 'react'
 import './Chat.css'
 
 function Chat() {
+
+    const sendMessage = () => {
+
+    }
     return (
         <div className="chat">
             
@@ -56,6 +60,15 @@ function Chat() {
                      </span>
                 </p>
 
+            </div>
+
+            <div className="chat__footer">
+                <InsertEmoticon/>
+                <form>
+                    <input placeholder="Type a message" type="text"/>
+                    <button type="submit">Send a message</button>
+                </form>
+                <MicNone/>
             </div>
         </div>
     )
