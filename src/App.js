9ef9be3a457b32f805faa36dba69eba98346ed70
Muhammad Muneeb Waseem <muneebwaseem78@gmail.com>
2021-06-30 23:18:44 +0500

    add axios.js for fetching and pusher to connect with frontend using useEffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
 import './App.css';
+import {useEffect, useState} from 'react'
 import Chat from './components/chat/Chat';
 import Sidebar from './components/sidebar/Sidebar';
+import Pusher from 'pusher-js'
+import axios from './axios'
 
 function App() {
+
+  const [messages,setMessages] = useState([]);
+
+  //useEffect for fetching
+
+  useEffect(()=>{
+    axios.get('/messages/sync')
+      .then(response=>{
+        setMessages(response.data)
+      })
+  }, [])
+
+  useEffect(()=>{
+    const pusher = new Pusher('1da9a361a566d7dc83bc', {
+      cluster: 'ap2'
+    });
+
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', (newMessage)=> {
+      setMessages([...messages, newMessage])
+    });
+
+    return ()=>{
+      channel.unbind_all(); // to unbind so it will no listen to all messages everytime, only listen new message
+      channel.unsubscribe();
+    };
+
+  }, [messages]) //add message here so it updates with the message
+
+  console.log(messages);
+
   return (
     <div className="app">
       <div className="app__body">
